4775a1c316023f5321933f4863708adc6f551d56
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-05-17 19:02:15 -0300

    Validacion de presupuesto y mensaje de error

--- src/components/NuevoPresupuesto.jsx
+++ src/components/NuevoPresupuesto.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import { useState } from 'react'
+//Importamos el componente de mensaje
+import Mensaje from './Mensaje'
 
 const NuevoPresupuesto = ({ presupuesto, setPresupuesto }) => {
+
+  const [mensaje, setMensaje] = useState('')
+  
+  const handlePresupuesto = (e) => {
+    e.preventDefault();
+
+    // Validamos que el monto ingresado sea de tipo numerico y positivo
+    if(!Number(presupuesto) || Number(presupuesto < 0)){
+      setMensaje('No es un presupuesto valido')
+    } else {
+      console.log('Si es un presupuesto valido')
+    }
+  }
+  
   return (
     <div className='contenedor-presupuesto contenedor sombra'>
-        <form className="formulario">
+        <form 
+          onSubmit={handlePresupuesto} 
+          className="formulario"
+        >
             <div className="campo">
                 <label>Definir Presupuesto</label>
 
@@ -17,6 +36,9 @@ const NuevoPresupuesto = ({ presupuesto, setPresupuesto }) => {
             </div>
 
             <input type="submit" value="Añadir" />
+
+            {/** Mensaje de error */}
+            {mensaje && <Mensaje tipo="error">{mensaje}</Mensaje>}
         </form>
     </div>
   )
