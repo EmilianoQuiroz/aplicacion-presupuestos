79764d4f7f9c4106d2e7a7767d4146bd346c42f9
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-05-18 22:39:05 -0300

    Formateo de numeros a dinero

--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
 
 const ControlPresupuesto = ({presupuesto}) => {
+
+    const formatearCantidad = (cantidad) => {
+        return cantidad.toLocaleString('en-US', {
+            style: 'currency',
+            currency: 'USD'
+        })
+    }
+
   return (
     <div className='contenedor-presupuesto contenedor sombra dos-columnas'>
         <div>
@@ -9,7 +17,15 @@ const ControlPresupuesto = ({presupuesto}) => {
 
         <div className='contenido-presupuesto'>
             <p>
-                <span>Presupuesto: </span> ${presupuesto}
+                <span>Presupuesto: </span> {formatearCantidad(presupuesto)}
+            </p>
+
+            <p>
+                <span>Disponible: </span> {formatearCantidad(0)}
+            </p>
+
+            <p>
+                <span>Gastado: </span> {formatearCantidad(0)}
             </p>
         </div>
     </div>
