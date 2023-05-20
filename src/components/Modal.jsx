a729feda83edafe87b093e93750b8343e049a11a
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-05-20 14:56:21 -0300

    Añadir formulario de gastos

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -22,6 +22,46 @@ const ocultarModal = () => {
 
         <form className={`formulario ${animarModal ? "animar" : 'cerrar'}`}>
           <legend>Nuevo gasto</legend>
+
+          <div className="campo">
+            <label htmlFor="nombre">Nombre Gastos</label>
+            <input 
+              id="nombre"
+              type='text'
+              placeholder='Añade el nombre del gasto'
+            />
+          </div>
+          
+          <div className="campo">
+            <label htmlFor="cantidad">Cantidad</label>
+            <input 
+              id="nombre"
+              type='number'
+              placeholder='Añade la cantidad del gasto ej: 300'
+            />
+          </div>
+
+          <div className="campo">
+            <label htmlFor="categoria">Categoria</label>
+            <select
+              id='categoria'
+            >
+              <option value="">Seleccione</option>
+              <option value="ahorro">Ahorro</option>
+              <option value="comida">Comida</option>
+              <option value="casa">Casa</option>
+              <option value="gastos">Gastos Varios</option>
+              <option value="ocio">Ocio</option>
+              <option value="salud">Salud</option>
+              <option value="suscripciones">Suscripciones</option>
+            </select>
+          </div>
+
+          <input 
+            type='submit'
+            value="Añadir gasto"
+          />
+
         </form>
     </div>
   )
