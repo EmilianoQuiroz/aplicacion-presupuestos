d3a4bde04f7140b5b5c2e574d7b9c3e2335f7fae
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-05-22 17:40:01 -0300

    Efecto Swipe

--- src/components/Gasto.jsx
+++ src/components/Gasto.jsx
@@ -1,4 +1,14 @@
 import React from 'react'
+// Importamos react-swipeable-list
+import {
+    LeadingActions,
+    SwipeableList,
+    SwipeableListItem,
+    SwipeAction,
+    TrailingActions
+} from 'react-swipeable-list'
+// Importamos los estilos de react-swipeable-list
+import 'react-swipeable-list/dist/styles.css'
 import { formatearFecha } from '../helpers'
 // Importamos los iconos de las categoroias de los gastos
 import IconoAhorro from '../img/icono_ahorro.svg'
@@ -23,25 +33,47 @@ const diccionarioIconos = {
 
 const Gasto = ({gasto}) => {
     const { categoria, nombre, cantidad, id, fecha } = gasto;
+    
+    const leadingActions = () => (
+        <LeadingActions>
+            <SwipeAction onClick={() => console.log('editar')}>
+                Editar
+            </SwipeAction>
+        </LeadingActions>
+    )
+    const trailingActions = () => (
+        <TrailingActions>
+            <SwipeAction onClick={() => console.log('Eliminar')}>
+                Eliminar
+            </SwipeAction>
+        </TrailingActions>
+    )
     return (
-    <div className="gasto sombra">
-        <div className="contenido-gasto">
-            {/** Imagen */}
-            <img 
-                src={diccionarioIconos[categoria]}
-                alt='iconogasto'
-            />
-            <div className="descripcion-gasto">
-                <p className="categoria">{gasto.categoria}</p>
-                <p className="nombre-gasto">{gasto.nombre}</p>
-                <p className="fecha-gasto">
-                    Agregado el: {''}
-                    <span>{formatearFecha(fecha)}</span>
-                </p>
+    <SwipeableList>
+        <SwipeableListItem
+        leadingActions={leadingActions()}
+        trailingActions={trailingActions()}
+        >
+            <div className="gasto sombra">
+                <div className="contenido-gasto">
+                    {/** Imagen */}
+                    <img 
+                        src={diccionarioIconos[categoria]}
+                        alt='iconogasto'
+                    />
+                    <div className="descripcion-gasto">
+                        <p className="categoria">{gasto.categoria}</p>
+                        <p className="nombre-gasto">{gasto.nombre}</p>
+                        <p className="fecha-gasto">
+                            Agregado el: {''}
+                            <span>{formatearFecha(fecha)}</span>
+                        </p>
+                    </div>
+                </div>
+                    <p className="cantidad-gasto">${cantidad}</p>
             </div>
-        </div>
-            <p className="cantidad-gasto">${cantidad}</p>
-    </div>
+        </SwipeableListItem>
+    </SwipeableList>
   )
 }
 
